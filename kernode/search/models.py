62c35e4270047bd47e4a1c8c0c8f1e7114a94c19
NEUPAP <41545040+NEUPAP@users.noreply.github.com>
2019-01-04 02:58:58 +0100

    models and admin interface

--- kernode/search/models.py
+++ kernode/search/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.URLField(max_length=255)
+    data = models.ImageField
+
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.URLField(max_length=255)
+    data = models.FileField
