e4ba019b875ce9778aa103fdda40a67a4dcda391
NEUPAP <41545040+NEUPAP@users.noreply.github.com>
2019-01-10 12:41:10 +0100

    create kerninfo

---
+++ kernode/search/kerninfo/media.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class Picture:
+    def __init__(self, image):
+        self.img = image
+        self.exif = {}
+    
+    def funcname(self, parameter_list):
+        pass
