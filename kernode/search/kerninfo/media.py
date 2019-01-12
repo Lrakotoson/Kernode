1bbc62b8cfe3da98f0a763867e4483e418cfe74e
NEUPAP <41545040+NEUPAP@users.noreply.github.com>
2019-01-12 22:52:02 +0100

    add gpsimgdirection and datestamp to media

--- kernode/search/kerninfo/media.py
+++ kernode/search/kerninfo/media.py
@@ -5,7 +5,7 @@ from PIL import Image as PI, ExifTags as PE
 class Picture:
     def __init__(self, image):
         self.img = PI.open(image)
-        self.exif = {}
+        self.exif = self._Pgetexif()
     
     def _Pgetexif(self):
         """
@@ -14,6 +14,7 @@ class Picture:
         3 - extract photograph info from exif
         return: ordered Picture exif 
         """
+        Ordexif = {}
         exif = {
             PE.TAGS[cle]:val
             for cle, val in getattr(self.img, '_getexif', lambda: None)().items()
@@ -26,10 +27,10 @@ class Picture:
             for cle in exif["GPSInfo"].keys():
                 data = PE.GPSTAGS.get(cle,cle)
                 gpsinfo[data] = exif["GPSInfo"][cle]
-            self.exif["Informations GPS"] = self._gpsconversion(gpsinfo)
+            Ordexif["Informations GPS"] = self._gpsconversion(gpsinfo)
             del exif["GPSInfo"]
         
-        pass
+        return Ordexif
     
     def _gpsconversion(self, gpsDMS):
         gpsDD = {}
@@ -47,27 +48,30 @@ class Picture:
         
         if "GPSAltitude" in gpsDMS:
             gAlt = gpsDMS["GPSAltitude"]
-            if int.from_bytes(gpsDMS["GPSAltitudeRef"],"big") == 0:
-                level = "above"
-            else:
+            if int.from_bytes(gpsDMS["GPSAltitudeRef"],"big") == 1:
                 level = "below"
+            else:
+                level = "above"
             gpsDD["Altitude"] = "{} meters {} sea level".format(gAlt[0]/gAlt[1],level)
 
         if "GPSTimeStamp" in gpsDMS:
             gTstp = gpsDMS["GPSTimeStamp"]
-            gpsDD["Timestamp"] = dt.datetime.strptime("{}:{}:{}".format(
+            gpsDD["Time stamp"] = dt.datetime.strptime("{}:{}:{}".format(
                                                 int(gTstp[0][0]/gTstp[0][1]),
                                                 int(gTstp[1][0]/gTstp[1][1]),
                                                 int(gTstp[2][0]/gTstp[2][1])),
                                                 "%H:%M:%S")
 
-
-
+        if "GPSDateStamp" in gpsDMS:
+            gpsDD["Date stamp"] = dt.datetime.strptime(gpsDMS["GPSDateStamp"],"%Y:%m:%d")
         
+        if "GPSImgDirection" in gpsDMS:
+            gImg = gpsDMS["GPSImgDirection"]
+            if gpsDMS["GPSImgDirectionRef"] == "M":
+                ref = "Magnetic"
+            else:
+                ref = "True"
+            gpsDD["Image Direction"] = "{} : ref {} North".format(gImg[0]/gImg[1], ref)
         
-
-#GPSImgDirectionRef  <class 'str'> : T  <class 'str'>
-#GPSImgDirection  <class 'str'> : (12222, 619)  <class 'tuple'>      
-
 
         return gpsDD
